9fc9a657237a86d4a6c4c816cb2e9bb590d84918
Jay Kidwell <jay@contensive.com>
2013-10-05 22:14:06 -0400

    Finished conversion, combined banners + lists

--- aoBannerCampaigns/aoBannerCampaigns/BannerCampaigns.cs
+++ aoBannerCampaigns/aoBannerCampaigns/BannerCampaigns.cs
@@ -4,30 +4,19 @@ using System.Linq;
 using System.Text;
 using Contensive.BaseClasses;
 
-namespace aoBannerCampaigns
+namespace Contensive.Addons.aoBannerCampaigns
 {
     public class BannerCampaigns : AddonBaseClass
     {
         public override object Execute(CPBaseClass cp)
         {
-            try
-            {
-                return getContent(cp);
-            }
-            catch (Exception ex)
-            {
-                cp.Site.ErrorReport(ex, "Unexpected trap");
-                return string.Empty;
-            }
-        }
-
-        private string getContent(CPBaseClass cp)
-        {
-            string stringValue = string.Empty;
-            string html = "starting getContent()";
+            string html = "";
 
             try
             {
+                DateTime dateMinValue = new DateTime( 1990, 1, 1);
+                string bannerTitle;
+                bool exitWithoutBanner = false;
                 CPCSBaseClass cs = cp.CSNew();
                 campaignID = cp.Doc.GetInteger("Campaign");
                 if (campaignID == 0)
@@ -46,133 +35,141 @@ namespace aoBannerCampaigns
                         cs.SetField("Clicks", (clicks + 1).ToString());
                         encodedLink = cs.GetText("Link");
                         nonEncodedLink = cp.Utils.DecodeResponseVariable(encodedLink);
+                        cp.Response.Redirect(nonEncodedLink);
+                        exitWithoutBanner = true;
                     }
                     cs.Close();
                 }
-                //==========================================================================
-                // get next banner
-                //==========================================================================
-                sQLNow = cp.Db.EncodeSQLDate(DateTime.Now);
-                isLinkAuthoring = cp.User.IsEditingAnything;
-                if (campaignID != 0)
+                if (!exitWithoutBanner)
                 {
-                    if (isLinkAuthoring)
+                    //==========================================================================
+                    // get next banner
+                    //==========================================================================
+                    sQLNow = cp.Db.EncodeSQLDate(DateTime.Now);
+                    isLinkAuthoring = cp.User.IsEditingAnything;
+                    if (campaignID != 0)
                     {
-                        bannerCriteria = getBannerCriteria(cp, cs, campaignID, false);
+                        if (isLinkAuthoring)
+                        {
+                            bannerCriteria = getBannerCriteria(cp, cs, campaignID, false);
+                        }
+                        else
+                        {
+                            // Campaign Name given no authoring - select a banner from the campaign
+                            bannerCriteria = string.Format("((ClicksMax is null)OR(Clicks<ClicksMax)) " +
+                                "AND((ViewingsMax is null)OR(Viewings<ViewingsMax))" +
+                                "AND((DateExpires is null)OR(DateExpires>{0})){1}", sQLNow, getBannerCriteria(cp, cs, campaignID, true));
+                        }
                     }
                     else
                     {
-                        // Campaign Name given no authoring - select a banner from the campaign
-                        bannerCriteria = string.Format("((ClickMax is null) IR (Click<ClickMax)) " +
-                            "AND((ViewingsMax is null)OR(Viewings<ViewingsMax))" +
-                            "AND((DateExpires is null)OR(DateExpires>'{0}')) '{1}'", sQLNow, getBannerCriteria(cp, cs, campaignID, true));
+                        //  no Campaign given, get any banner from any campaign
+                        bannerCriteria = string.Format("((ClicksMax is null)OR(Clicks<ClicksMax))" +
+                                                       "AND((ViewingsMax is null)OR(Viewings<ViewingsMax))" +
+                                                       "AND((DateExpires is null)OR(DateExpires>{0}))", sQLNow);
+
                     }
-                }
-                else
-                {
-                    //  no Campaign given, get any banner from any campaign
-                    bannerCriteria = string.Format("((ClicksMax is null)OR(Clicks<ClicksMax))" +
-                                                   "AND((ViewingsMax is null)OR(Viewings<ViewingsMax))" +
-                                                   "AND((DateExpires is null)OR(DateExpires>'{0}'))", sQLNow);
+                    if (cs.Open("Banners", bannerCriteria, "LastViewDate", true, string.Empty, 10, 1))
+                    {
+                        bannerName = cs.GetText("name");
+                        bannerID = cs.GetInteger("ID");
+                        link = cs.GetText("Link");
+                        newWindow = cs.GetBoolean("NewWindow");
+                        height = cs.GetInteger("Height");
+                        width = cs.GetInteger("Width");
+                        align = cs.GetText("Align");
+                        clicks = cs.GetInteger("Clicks");
+                        clicksMax = cs.GetInteger("ClicksMax");
+                        viewings = cs.GetInteger("Viewings");
+                        viewingsMax = cs.GetInteger("ViewingsMax");
+                        dateExpires = cs.GetDate("DateExpires");
+                        bannerTitle = cs.GetText("caption");
+                        qS = cp.Doc.RefreshQueryString;
 
-                }
-                if (cs.Open("Banners", bannerCriteria, "LastViewDate", true, string.Empty, 10, 1))
-                {
-                    bannerID = cs.GetInteger("ID");
-                    link = cs.GetText("Link");
-                    newWindow = cs.GetBoolean("NewWindow");
-                    height = cs.GetInteger("Height");
-                    width = cs.GetInteger("Width");
-                    align = cs.GetText("Align");
-                    clicks = cs.GetInteger("Clicks");
-                    clicksMax = cs.GetInteger("ClicksMax");
-                    viewings = cs.GetInteger("Viewings");
-                    viewingsMax = cs.GetInteger("ViewingsMax");
-                    dateExpires = cs.GetDate("DateExpires");
-                    qS = cp.Request.QueryString;
+                        if (isLinkAuthoring)
+                        {
+                            html += string.Format("<div>{0}</div>", cs.GetEditLink());
+                        }
 
+                        if (!string.IsNullOrEmpty(link))
+                        {
+                            qS = cp.Utils.ModifyQueryString(qS, "bannerid", bannerID.ToString(), true);
+                            html += string.Format("<a href=\"{0}?{1}\"", cp.Request.Page, qS);
+                            if (newWindow)
+                            {
+                                html += " target=\"_blank\"";
+                            }
+                            html += ">";
+                        }
+
+                        // ----- Add banner
+                        nonEncodedLink = cp.Request.Protocol + cp.Request.Host + cp.Site.FilePath + cs.GetText("ImageFilename");
+                        encodedLink = cp.Utils.EncodeHTML(nonEncodedLink);
+                        html += string.Format("<IMG border=\"0\" src=\"{0}\"", encodedLink);
+                        if (width != 0)
+                        {
+                            html += string.Format(" width=\"{0}\"", width);
+                        }
+                        if (height != 0)
+                        {
+                            html += string.Format(" height=\"{0}\"", height);
+                        }
+                        if (!string.IsNullOrEmpty(align))
+                        {
+                            html += string.Format(" align=\"{0}\"", align);
+                        }
+
+                        html += string.Format(" alt=\"{0}\" title=\"{0}\">", cp.Utils.EncodeHTML(bannerTitle));
+
+                        if (!string.IsNullOrEmpty(link))
+                        {
+                            html += "</a>";
+                        }
+
+                        if (!string.IsNullOrEmpty(bannerName))
+                        {
+                            if (clicksMax != 0 && clicks > clicksMax)
+                            {
+                                hiddenResponse = string.Format("<div style=\"clear:both;\">[Administrator, Clicks Max have been met for this banner: {0}]</div>", bannerName);
+                            }
+                            else if (viewingsMax != 0 && viewings > viewingsMax)
+                            {
+                                hiddenResponse = string.Format("<div style=\"clear:both;\">[Administrator, Viewings Max have been met for this banner: {0}]</div>", bannerName);
+                            }
+                            else if ((dateExpires > dateMinValue) && (dateExpires < DateTime.Now))
+                            {
+                                hiddenResponse = string.Format("<div style=\"clear:both;\">[Administrator, this banner has expired: {0}]</div>", bannerName);
+                            }
+                        }
+                        if (!isLinkAuthoring)
+                        {
+                            // not authoring and banner found - count viewing and update lastviewdate
+                            // this means in LinkAuthoring, banner will not rotate
+                            //  Also add a viewing record for the banner so we can track individual viewing dates
+                            cs.SetField("Viewings", (viewings + 1).ToString());
+                            cs.SetField("LastViewDate", DateTime.Now.ToString());
+                            if (cs.Insert("Banner Viewings"))
+                            {
+                                cs.SetField("BannerID", bannerID.ToString());
+                            }
+                        }
+                    }
+                    cs.Close();
+                    //
+                    // administrator - add a banner
+                    //
                     if (isLinkAuthoring)
                     {
-                        stringValue += string.Format("<div>{0}</div>", cs.GetEditLink(true));
-                    }
-
-                    if (!string.IsNullOrEmpty(link))
-                    {
-                        qS = cp.Utils.ModifyQueryString(qS, "bannerid", bannerID.ToString(), true);
-                        html += string.Format("<a href='{0}'?'{1}'", cp.Request.Page, qS);
-                        if (newWindow)
-                        {
-                            html += "target='_blank'";
-                        }
-                        html += "/>";
-                    }
-
-                    // ----- Add banner
-                    nonEncodedLink = cp.Request.Protocol + cp.Site.FilePath + cs.GetText("ImageFilename");
-                    encodedLink = cp.Utils.EncodeHTML(nonEncodedLink);
-                    html += string.Format("<IMG border='0' src='{0}'", encodedLink);
-                    if (width != 0)
-                    {
-                        html += string.Format("width='{0}'", width);
-                    }
-                    if (height != 0)
-                    {
-                        html += string.Format("height='{0}'", height);
-                    }
-                    if (!string.IsNullOrEmpty(align))
-                    {
-                        html += string.Format("align='{1}'", align);
-                    }
-
-                    if (!string.IsNullOrEmpty(link))
-                    {
-                        html += "</a>";
-                    }
-
-                    html = string.Format("alt='{0}'", align);
-
-
-                    if (!string.IsNullOrEmpty(bannerName))
-                    {
-                        if (clicksMax != 0 && clicks > clicksMax)
-                        {
-                            hiddenResponse = string.Format("[Banner hidden (Clicks Max Met): {0}]", bannerName);
-                        }
-                        else if (viewings != 0 && viewings > viewingsMax)
-                        {
-                            hiddenResponse = string.Format("[Banner hidden (Viewings Max Met): {0}]", bannerName);
-                        }
-                        else if (dateExpires < DateTime.Now)
-                        {
-                            hiddenResponse = string.Format("[Banner hidden (Expired): {0}]", bannerName);
-                        }
+                        // authoring - show hiddenresponse and do not count viewings or lastviewdate
+                        html += hiddenResponse;
+                        string addLink = cp.Content.GetAddLink("banners", "campaign=" + campaignID, false, true);
+                        html += string.Format("<div style=\"clear:both;\">{0}&nbsp;Add a banner</div>", addLink);
                     }
                 }
-
-                if (isLinkAuthoring)
-                {
-                    // authoring - show hiddenresponse and do not count viewings or lastviewdate
-                    html += hiddenResponse;
-                    html += string.Format("<div>{0}nbsp;Add a banner</div>", cs.GetAddLink("Banners,campaign=" + campaignID));
-                }
-                else
-                {
-                    // not authoring and banner found - count viewing and update lastviewdate
-                    // this means in LinkAuthoring, banner will not rotate
-                    //  Also add a viewing record for the banner so we can track individual viewing dates
-                    cs.SetField("Viewings", (viewings + 1).ToString());
-                    cs.SetField("LastViewDate", DateTime.Now.ToString());
-                    if (cs.Insert("Banner Viewings"))
-                    {
-                        cs.SetField("BannerID", bannerID.ToString());
-                    }
-                }
-                cs.Close();
-
             }
             catch (Exception ex)
             {
-                cp.Site.ErrorReport(ex, "Unexpected trap");
+                cp.Site.ErrorReport(ex, "Unexpected trap in execute");
                 return string.Empty;
             }
             return html;
@@ -212,12 +209,12 @@ namespace aoBannerCampaigns
 
                 cs.Close();
 
-                stream += string.Format("(ID IN('{0})')", innerStream);
+                stream += string.Format("(ID IN({0}))", innerStream);
 
             }
             catch (Exception ex)
             {
-                cp.Site.ErrorReport(ex, "Unexpected trap");
+                cp.Site.ErrorReport(ex, "Unexpected trap in getBannerCriteria");
                 return string.Empty;
             }
 
